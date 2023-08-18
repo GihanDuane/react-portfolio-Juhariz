3451cdda36e5470bc8394f76baa9781a0637ed2f
Gihan Duane <duanegaid.102094@gmail.com>
2023-08-18 12:17:59 +0800

    added pharagraph line on about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,13 @@ const About = () => {
   return (
     <div className="about">
       <h2>About Kheil Artimist</h2>
-      <p>Insert a brief description about Kheil Artimist here.</p>
+      <p>
+        Kheil Artimist is an illustrator and character designer based in the Philippines. He has been drawing since he was a child, and his passion for art has only grown stronger over the years. He is inspired by a wide range of art styles, from anime and manga to western comics and cartoons.
+        Kheil's artwork is characterized by its vibrant colors, dynamic poses, and expressive characters. He loves to create stories through his art, and he often incorporates elements of fantasy and mythology into his work.
+        In addition to his illustration work, Kheil is also a talented concept artist. He has created concept art for a variety of projects, including video games, animated films, and comic books.
+        Kheil is always looking for new challenges, and he is eager to learn new techniques and styles. He is a dedicated artist who is passionate about his work, and he is always striving to improve his skills.
+        If you are looking for an illustrator who can bring your vision to life, then Kheil Artimist is the artist for you. He is a talented and creative artist who is sure to exceed your expectations.
+      </p>
     </div>
   );
 };
