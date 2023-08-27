ddad4a7bc1db1cf81a8ad20903d838d368b93387
Gihan Duane <duanegaid.102094@gmail.com>
2023-08-27 15:44:42 +0800

    created header and navalbum component, add css to it to make it center

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import NavAlbum from './components/NavAlbum';
 // import ArtworkGrid from './components/ArtworkGrid';
 import './style.css'; // Import your custom CSS here
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <NavAlbum />
       {/* <ArtworkGrid /> */}
       {/* <footer>&copy; 2023 Kheil Artimist. All rights reserved.</footer> */}
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,57 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <div className="logo">Juhariz Pandog/Kheil Artimist</div>
+      <div className="">
+        <div className="logo">Juhariz Pandog/Kheil Artimist</div>
+        <p>Freelancer Illustrator/Character Concept designer</p>
+      </div>
+      
       <ul>
         <li><a href="#works">Home</a></li>
         <li><a href="#works">Resume</a></li>
       </ul>
     </header>
+    // <header class="site-header">
+    //             <div class="container-lg">
+    //                 <div class="row row-middle">
+    //                     <div class="site-header-col-left">
+    //                         <div class="site-title title-font">
+    //                             <a href="/">Juhariz Pandog/Kheil Artimist</a>
+    //                             <div class="site-subtitle visible-desktop">Freelance Illustrator/Character Concept designer</div>
+    //                         </div>
+    //                     </div>
+    //                     <div class="site-header-col-right text-right">
+    //                         <div class="hidden-desktop site-header-menu-btn" data-responsive-nav-toggle-btn>
+    //                             <div class="menu-icon">
+    //                                 <span class="menu-icon-line"></span>
+    //                                 <span class="menu-icon-line"></span>
+    //                                 <span class="menu-icon-line"></span>
+    //                             </div>
+    //                         </div>
+    //                         <div class="visible-desktop">
+    //                             <div class="header-nav">
+    //                                 <div class="header-nav-item active">
+    //                                     <a target="_self" href="/">Home</a>
+    //                                 </div>
+    //                                 <div class="header-nav-item">
+    //                                     <a target="_self" href="/resume">Resume</a>
+    //                                 </div>
+    //                             </div>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //                 <div class="responsive-nav-container hidden-desktop">
+    //                     <ul class="nav header-nav">
+    //                         <li class="header-nav-item active">
+    //                             <a target="_self" href="/">Home</a>
+    //                         </li>
+    //                         <li class="header-nav-item">
+    //                             <a target="_self" href="/resume">Resume</a>
+    //                         </li>
+    //                     </ul>
+    //                 </div>
+    //             </div>
+    //         </header>
   );
 };
 
