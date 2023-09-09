445518c22576fcbb7bf8ad2d88bbc05a67b7ad50
Gihan Duane <duanegaid.102094@gmail.com>
2023-09-09 22:24:56 +0800

    import image from assets to resume and added container to img tag

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,5 +1,11 @@
+import Myimage from "../assets/Profilepicture.jpg"
+
 export default function Resume() {
     return (
-        <div>About</div>
+        <div className="container-sm">
+            <div className="about-photo">
+            <img width={160} height={160} src={Myimage} alt="Juhariz Pandog/Kheil Artimist"/>
+            </div>
+        </div>
     )
 }
