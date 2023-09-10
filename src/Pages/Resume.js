bd365909cc7b5eacdc387b2608932d4fe35dd14f
Gihan Duane <duanegaid.102094@gmail.com>
2023-09-10 14:30:34 +0800

    finish the structure of about-intro with image

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -4,8 +4,12 @@ export default function Resume() {
     return (
         <div className="container-sm">
             <div className="about-photo">
-            <img width={160} height={160} src={Myimage} alt="Juhariz Pandog/Kheil Artimist"/>
+                <img width={160} height={160} src={Myimage} alt="Juhariz Pandog/Kheil Artimist"/>
             </div>
+            <div>Juhariz Pandog/Kheil Artimist</div>
+            <div>Freelancer Illustrator/Character Concept designer</div>
+            <div><span></span>South Cotabato, Philippines</div>
+            <div><a href="mailto:malcolnkheil@gmail.com">malcolnkheil@gmail.com</a></div>
         </div>
     )
 }
