c22af34024e670f160212270dcf19796683b3995
Gihan Duane <duanegaid.102094@gmail.com>
2023-09-10 18:07:25 +0800

    update the resume page

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,15 +1,7 @@
-import Myimage from "../assets/Profilepicture.jpg"
+import AboutPage from "../components/AboutPage"
 
 export default function Resume() {
     return (
-        <div className="container-sm">
-            <div className="about-photo">
-                <img width={160} height={160} src={Myimage} alt="Juhariz Pandog/Kheil Artimist"/>
-            </div>
-            <div>Juhariz Pandog/Kheil Artimist</div>
-            <div>Freelancer Illustrator/Character Concept designer</div>
-            <div><span></span>South Cotabato, Philippines</div>
-            <div><a href="mailto:malcolnkheil@gmail.com">malcolnkheil@gmail.com</a></div>
-        </div>
+        <AboutPage />
     )
 }
